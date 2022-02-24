69f171c916e95f4a162891b51b6d9ce1f95df961
zhengzhengcool <zhengzhengcool@qq.com>
2022-02-24 17:09:03 +0800

    首次添加 实现简单的碎片替换

---
+++ app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.fragmenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        replaceFragment(new RightFragment());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            case R.id.button2:
+                replaceFragment(new RightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+}
